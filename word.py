71ba9a559d5d7b7190008f50a8496f9d4898a1ca
Christian Ramiro <chris05ram@gmail.com>
2017-06-22 13:37:57 -0700

    Update word.py

--- word.py
+++ word.py
@@ -35,48 +35,6 @@ class Word:
             self.num_exist_senses = len([s for s in self.senses if s.end_date == 3000])
             self.nb_dates = [nb_sense.date for nb_sense in self.nb_senses]
 
-    def make_sense_list(self, label, part_of_speech):
-        """Takes a word and searches for it within the HTE database,
-        returning a list with the same information in a sense
-        object with, ordered a date, word_form, identifiers, and
-        categories"""
-
-        if part_of_speech == "a":
-            part_of_speech = "aj"
-        if part_of_speech == "adv":
-            part_of_speech = "av"
-
-        if part_of_speech == "v":
-            url = "http://historicalthesaurus.arts.gla.ac.uk/category-selection/?word=" + label + "&pos%5B%5D=allv&pos%5B%5D=v&pos%5B%5D=vi&pos%5B%5D=vm&pos%5B%5D=vp&pos%5B%5D=vr&pos%5B%5D=vt&label=&category=&startf=&endf=&startl=&endl="
-        elif part_of_speech == "all":
-            url = "http://historicalthesaurus.arts.gla.ac.uk/category-selection/?word=" + label + "&label=&category=&startf=&endf=&startl=&endl="
-        else:
-            url = "http://historicalthesaurus.arts.gla.ac.uk/category-selection/?word=" + label + "&pos%5B%5D=" + part_of_speech + "&label=&category=&startf=&endf=&startl=&endl="
-
-        r = requests.get(url)
-        soup = bs4.BeautifulSoup(r.content, "html.parser")
-        for a in soup.find_all("a"):
-            del a['href']
-
-        catOdds, catEvens = soup.find_all("p", "catOdd"), soup.find_all("p", "catEven")
-        allCat = catOdds + catEvens
-
-        sense_list = []
-
-        for index in range(0, len(allCat)):
-            indexed_sense = Sense(label, allCat, index)
-            # Skip past the current selection if the date is not a starting date
-            if not indexed_sense.date:
-                continue
-            sense_list.append(indexed_sense)
-
-        # this separation is done so that we can sort the list with OE at the beginning instead of the end
-        OE_list = [sense for sense in sense_list if sense.date == "OE"]
-        non_OE_list = [sense for sense in sense_list if sense.date != "OE"]
-        sense_list = OE_list + sorted(non_OE_list, key=lambda x: x.date)
-
-        return sense_list
-
     def __repr__(self):
         return self.label
 
