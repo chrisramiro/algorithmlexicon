363d4ddffc6a6e8c6f12cb92461b1fda4bfbc75f
Christian Ramiro <chris05ram@gmail.com>
2017-12-03 20:58:59 -0800

    Added sim scores and corresponding code

---
+++ word_similarity/sim_compare.py
@@ -0,0 +1,24 @@
+
+from openpyxl import Workbook, load_workbook
+import scipy.stats as stats
+
+def do_print(wb, end):
+    sim = load_workbook(wb)
+    ws = sim.active
+    score = []
+    f, d, dr = [], [], []
+    maxf = []
+
+    for i in range(2, end):
+        if ws.cell(row=i, column=5).value != ":(" and ws.cell(row=i, column=5).value:
+            score.append(ws.cell(row=i, column=4).value)
+            f.append(ws.cell(row=i, column=5).value)
+            d.append(ws.cell(row=i, column=6).value)
+            maxf.append(ws.cell(row=i, column=7).value)
+
+    print(wb)
+    print("d mean: " + str(stats.spearmanr(score, f)))
+    print("d max: " + str(stats.spearmanr(score, d)))
+    print("f max " + str(stats.spearmanr(score, maxf)))
+
+do_print("similarity_scores.xlsx", 355)
